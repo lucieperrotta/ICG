28b040a37866f09196fbd43853c5dac850e71149
Lucie <lucie.perrotta@epfl.ch>
2017-03-02 14:27:36 +0100

    Lab2 1st implementation

---
+++ build-lab2-Desktop-Default/lab2_intro_to_glsl/CMakeFiles/lab2_intro_to_glsl.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab2_intro_to_glsl.dir/main.cpp.obj"
+  "lab2_intro_to_glsl.pdb"
+  "lab2_intro_to_glsl.exe"
+  "lab2_intro_to_glsl.exe.manifest"
+  "lab2_intro_to_glsl.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab2_intro_to_glsl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
