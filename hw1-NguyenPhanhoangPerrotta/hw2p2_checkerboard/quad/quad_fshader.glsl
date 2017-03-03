4d53f7e8d2b09edcec3c864a6cbf7cb8b309c4fd
KL <kim.phanhoang@epfl.ch>
2017-03-03 20:46:06 +0100

    Exo2 hwk1 done

--- hw1-NguyenPhanhoangPerrotta/hw2p2_checkerboard/quad/quad_fshader.glsl
+++ hw1-NguyenPhanhoangPerrotta/hw2p2_checkerboard/quad/quad_fshader.glsl
@@ -1,13 +1,18 @@
 #version 330
 
 in vec2 uv;
-
 out vec3 color;
+const float PI = 3.141592;
 
 uniform sampler1D colormap;
 
 void main() {
-    color = vec3(1.0, 0.0, 0.0);
+
+    float v = sin(uv.x*10*PI); // 2pi*5 = 5 periods
+    float w = sin(uv.y*10*PI);
+    float value = (v*w) + 0.5; // +0.5 : to make the green squares bigger
+
+    color = texture(colormap, value).rgb;
 }
 
 
