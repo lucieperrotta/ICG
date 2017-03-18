d6ca541f757510b247b65eda2056b802ad498540
KL <kim.phanhoang@epfl.ch>
2017-03-18 21:37:43 +0100

    flat done

--- hw3-NguyenPhanhoangPerrotta/icg17/hw3_shading/mesh/flat_fshader.glsl
+++ hw3-NguyenPhanhoangPerrotta/icg17/hw3_shading/mesh/flat_fshader.glsl
@@ -1,26 +1,39 @@
 #version 330
 
+uniform vec3 La, Ld, Ls;
+uniform vec3 ka, kd, ks;
+uniform float alpha;
+uniform mat4 projection;
+uniform mat4 model;
+uniform mat4 view;
+uniform vec3 light_pos;
 
 out vec3 color;
-
+in vec3 light_dir;
+in vec3 view_dir;
+in vec4 vpoint_mv;
 
 void main() {
-    color = vec3(0.0,0.0,0.0);
-    const vec3 COLORS[6] = vec3[](
-        vec3(1.0,0.0,0.0),
-        vec3(0.0,1.0,0.0),
-        vec3(0.0,0.0,1.0),
-        vec3(1.0,1.0,0.0),
-        vec3(0.0,1.0,1.0),
-        vec3(1.0,0.0,1.0));
-    int index = int( mod(gl_PrimitiveID,6) );
-    color = COLORS[index];
 
     ///>>>>>>>>>> TODO >>>>>>>>>>>
     /// TODO 4.2: Flat shading.
     /// 1) compute triangle normal using dFdx and dFdy
+    vec3 x = dFdx(vpoint_mv).xyz;
+    vec3 y = dFdy(vpoint_mv).xyz;
+    vec3 normal_mv = cross(x,y);
+
     /// 1) compute ambient term.
     /// 2) compute diffuse term.
     /// 3) compute specular term.
-    ///<<<<<<<<<< TODO <<<<<<<<<<<
+    color += La*ka;
+    vec3 n = normalize(normal_mv);
+    vec3 l = normalize(light_dir);
+
+    float lambert = dot(n,l);
+    if(lambert > 0.0) {
+        color += Ld*kd*lambert;
+        vec3 v = normalize(view_dir);
+        vec3 r = reflect(-l,n);
+        color += Ls*ks*pow(max(dot(r,v), 0.0), alpha);
+    }
 }
