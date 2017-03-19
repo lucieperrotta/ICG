9e79c51fa6b793da7729f6637921ec7515add03b
Lucie <lucie.perrotta@epfl.ch>
2017-03-19 11:43:47 +0100

    Spotlight done

--- hw3-NguyenPhanhoangPerrotta/icg17/hw3_shading/mesh/spot_fshader.glsl
+++ hw3-NguyenPhanhoangPerrotta/icg17/hw3_shading/mesh/spot_fshader.glsl
@@ -1,25 +1,51 @@
 #version 330
 
+in vec3 light_dir;
+in vec3 spot_dir;
+in vec3 view_dir;
+in mat4 MV;
+in vec3 normal_mv;
 
 out vec3 color;
 
+uniform vec3 La, Ld, Ls;
+uniform vec3 ka, kd, ks;
+uniform float alpha;
 
-const float spot_cos_cutoff = 0.985; // cos 10 degrees
+const float spot_cos_cutoff = 0.99; // cos <10 degrees
 const float spot_exp = 150;
 
 void main() {    
-    const vec3 COLORS[6] = vec3[](
-        vec3(1.0,0.0,0.0),
-        vec3(0.0,1.0,0.0),
-        vec3(0.0,0.0,1.0),
-        vec3(1.0,1.0,0.0),
-        vec3(0.0,1.0,1.0),
-        vec3(1.0,0.0,1.0));
-    int index = int( mod(gl_PrimitiveID,6) );
-    color = COLORS[index];
 
-    //>>>>>>>>>> TODO >>>>>>>>>>>
-    // TODO 5: Spot light.
-    // Complete the shader to obtain spot light effect
-    //<<<<<<<<<< TODO <<<<<<<<<<<
+    // Normalizations
+    vec3 n = normalize(normal_mv);
+    vec3 l = normalize(light_dir);
+    vec3 v = normalize(view_dir);
+    vec3 s = normalize(spot_dir);
+
+    // ambient term
+    vec3 ambient = ka * La;
+    color += ambient;
+
+    float spotEffect = dot(-l,s);
+
+    if( spotEffect > spot_cos_cutoff){
+
+        float lambert = dot(n,l);
+
+        if(lambert > 0.0) {
+            // diffuse term
+            vec3 diffuse = Ld*kd*lambert;
+            color += diffuse;
+
+            // specular term
+            vec3 reflect = reflect(-l, n);
+            vec3 specular = Ls*ks*pow(max(dot(reflect,v), 0.0), alpha);
+            color += specular;
+        }
+
+        // Actual spotlight
+        spotEffect = pow(spotEffect, spot_exp);
+        color *= spotEffect;
+    }
 }
