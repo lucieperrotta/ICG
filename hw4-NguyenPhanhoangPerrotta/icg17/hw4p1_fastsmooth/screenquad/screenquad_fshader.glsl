cbeaabf2cd839028f4c9e42b05fe911627ac1747
Akynna <tim.nguyen@epfl.ch>
2017-03-30 00:49:52 +0200

    Last changes

--- hw4-NguyenPhanhoangPerrotta/icg17/hw4p1_fastsmooth/screenquad/screenquad_fshader.glsl
+++ hw4-NguyenPhanhoangPerrotta/icg17/hw4p1_fastsmooth/screenquad/screenquad_fshader.glsl
@@ -5,36 +5,37 @@ in vec2 uv;
 out vec3 color;
 
 uniform sampler2D tex;
-
 uniform float tex_width;
 uniform float tex_height;
 
 uniform int kernel_size;
-uniform float kernel[400];
+uniform float kernel[450];
+
 uniform int axis;
 
 void main() {
 
+     // gaussian convolution
     float weight_tot = 0.0;
+    float middle = kernel_size / 2.0;
+
     vec3 color_tot = vec3(0.0);
     vec3 neigh_color = vec3(0.0);
-    vec2 offset = vec2(0.0);
 
     for(int i = 0; i < kernel_size; i++){
-       float weight = kernel[i];
 
        if(axis == 0) { // pass for the x axis
-            offset = vec2((i - (kernel_size / 2.0)) / tex_width, 0.0);
+           neigh_color = texture(tex, uv + vec2((i - middle) / tex_width, 0.0)).rgb;
        } else {
-            offset = vec2(0.0, (i - (kernel_size / 2.0)) / tex_height);
+           neigh_color = texture(tex, uv + vec2(0.0, (i - middle) / tex_height)).rgb;
        }
 
-       neigh_color = texture(tex, uv + offset).rgb;
-       color_tot += weight * neigh_color;
-       weight_tot += weight;
+
+       color_tot += kernel[i] * neigh_color;
+       weight_tot += kernel[i];
     }
 
-    color = color_tot / weight_tot;
+    color = color_tot / weight_tot; // ensure \int w = 1
 
 }
 
