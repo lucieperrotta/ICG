6784624d6a10d8d1fb1591919b4fc8d733b7135e
Akynna <tim.nguyen@epfl.ch>
2017-05-19 14:42:50 +0200

    Fixed textures

--- Project/icg17/project/grid/grid_fshader.glsl
+++ Project/icg17/project/grid/grid_fshader.glsl
@@ -20,6 +20,8 @@ uniform float offsetX;
 uniform float offsetY;
 
 // lanscape values
+// float lake_level = 0.25f;
+// float height_scale = 0.7;
 uniform float lake_level;
 uniform float height_scale;
 
@@ -31,20 +33,20 @@ in float transparency;
 
 out vec4 color;
 
-vec3 color1 = vec3(0,0,0);
+vec3 raw_color = vec3(0,0,0);
 
 
-void choosecolor1OnHeight(float height, vec3 normal_mv) {
+void chooseraw_colorOnHeight(float height, vec3 normal_mv) {
 
-    float sand_level = lake_level + 0.02*height_scale;
-    float grass_level = sand_level + 0.15*height_scale;
-    float mountains_level = grass_level + 0.2*height_scale;
+    float sand_level = lake_level + 0.1*height;
+    float grass_level = sand_level + 0.1*height;
+    float mountains_level = grass_level + 0.3*height;
 
     // Initalize the texture vectors and the blending factor
     vec4 t1 = vec4(0.0);
     vec4 t2 = vec4(0.0);
     float blend_factor = 0.0;
-    float scale_factor = 18; // used to make textures smaller so we can repeat them
+    float scale_factor = 25; // used to make textures smaller so we can repeat them
 
     vec2 uv_offset_scale = (uv + (vec2(offsetX, offsetY)+1)/1.3f)*scale_factor;
 
@@ -52,12 +54,12 @@ void choosecolor1OnHeight(float height, vec3 normal_mv) {
         // Compute the blend factor which depends on the height
         blend_factor = (height - lake_level)/(sand_level - lake_level);
 
-        t1 = 0.85*texture(tex_sand, uv_offset_scale);
+        t1 = 0.75*texture(tex_sand, uv_offset_scale);
         t2 = texture(tex_sand, uv_offset_scale);
-        color1 = mix(t1, t2, blend_factor).xyz;
+        raw_color = mix(t1, t2, blend_factor).xyz;
     } else if(height <= sand_level) {
         // Sand & Lake level
-        color1 = texture(tex_sand, uv_offset_scale).xyz;
+        raw_color = texture(tex_sand, uv_offset_scale).xyz;
     } else if(height <= grass_level) {
         // Grass & Forest level
         t1 = texture(tex_sand, uv_offset_scale);
@@ -67,13 +69,13 @@ void choosecolor1OnHeight(float height, vec3 normal_mv) {
         blend_factor = (height - sand_level)/(grass_level - sand_level);
 
         if(normal_mv.x < 0.2) {
-            color1 = t1.xyz;
+            raw_color = t1.xyz;
         } else {
-            color1 = t2.xyz;
+            raw_color = t2.xyz;
         }
 
-        // Equivalent to color1 = (t1*(1-grass_height) + t2*grass_height).xyz;
-        color1 = mix(t1, t2, blend_factor).xyz;
+        // Equivalent to raw_color = (t1*(1-grass_height) + t2*grass_height).xyz;
+        raw_color = mix(t1, t2, blend_factor).xyz;
     } else if(height <= mountains_level) {
         // Rock & Mountains Level
         t1 = texture(tex_grass, uv_offset_scale);
@@ -82,7 +84,7 @@ void choosecolor1OnHeight(float height, vec3 normal_mv) {
         // Compute the blend factor which depends on the height
         blend_factor = (height - grass_level)/(mountains_level - grass_level);
 
-        color1 = mix(t1, t2, blend_factor).xyz;
+        raw_color = mix(t1, t2, blend_factor).xyz;
     } else {
         // Snow of moutains level
         t1 = texture(tex_rock, uv_offset_scale);
@@ -91,7 +93,7 @@ void choosecolor1OnHeight(float height, vec3 normal_mv) {
         // Compute the blend factor which depends on the height
         float snow_height = (height - mountains_level)/(1.0 - mountains_level);
 
-        color1 = mix(t1, t2, snow_height).xyz;
+        raw_color = mix(t1, t2, snow_height).xyz;
     }
 
 }
@@ -115,19 +117,19 @@ void main() {
     vec3 normal_mv = vec3(MV * vec4(normal, 0));
 
 
-    choosecolor1OnHeight(height, normal_mv);
+    chooseraw_colorOnHeight(height, normal_mv);
 
 
 
     // PHONG SHADING
-    //color1=vec3(0);
+    //raw_color=vec3(0);
 
     vec3 light_dir = light_pos - vpoint_mv.xyz;
     vec3 view_dir = -vpoint_mv.xyz;
 
     // ambient term
     vec3 ambient = ka * La;
-    //color1 += ambient;
+    //raw_color += ambient;
 
     vec3 n = normalize(normal_mv);
     vec3 l = normalize(light_dir);
@@ -137,14 +139,14 @@ void main() {
 
         // diffuse term
         vec3 diffuse = Ld*kd*lambert;
-        color1 += diffuse;
+        raw_color += diffuse;
 
         // specular term
         vec3 v = normalize(view_dir);
         vec3 reflect = reflect(-l, n);
         vec3 specular = Ls*ks*pow(max(dot(reflect,v), 0.0), alpha);
-        //color1 += specular;
+        //raw_color += specular;
     }
     //transparency for direant pixels
-    color=vec4(color1, transparency);
+    color = vec4(raw_color, transparency);
 }
